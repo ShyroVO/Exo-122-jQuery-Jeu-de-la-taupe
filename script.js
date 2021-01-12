634ddf9841400fc88614eb7aed3526f145b119c6
ShyroVO <bosquet.laurie@outlook.com>
2021-01-12 13:48:33 +0100

    0.2 / Abandon Temporaire

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole'); // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = $('.score'); //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = $('.mole'); // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+
+document.getElementById('startGame').addEventListener('click', startGame);
+
+let randomTrou = Math.floor(Math.random() * Math.floor(holes.length));
+let randomTrouName = "hole" + randomTrou;
+console.log(randomTrouName);
+
+// Start:
+function startGame(){
+    console.log('start');
+
+}
+
+// Test moles:
+$(randomTrouName).addClass('.up');
+
